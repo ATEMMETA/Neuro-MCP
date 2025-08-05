111f4ed39a675c6eacf942e6a921ee8c10f18d3b
ATEMMETA <atem.86796@gmail.com>
2025-08-05 18:43:54 -0500

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+// jest.config.js
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: [
+    "**/__tests__/**/*.ts",
+    "**/?(*.)+(spec|test).ts"
+  ],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["json", "lcov", "text", "clover"],
+  verbose: true
+};
